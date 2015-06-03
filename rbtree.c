9bf86aad3903e78ced8699710b708bbfece9bb0c
Katharina Sabel <sabel.katharina@gmail.com>
2015-06-03 17:52:40 +0200

    Initial commit of the project

---
+++ rbtree.c
@@ -0,0 +1,5 @@
+//
+// Created by spacekookie on 03/06/15.
+//
+
+#include "rbtree.h"
---
+++ rbtree.h
@@ -0,0 +1,8 @@
+//
+// Created by spacekookie on 03/06/15.
+//
+
+#ifndef UNI_PROJECT_RBTREE_H
+#define UNI_PROJECT_RBTREE_H
+
+#endif //UNI_PROJECT_RBTREE_H
